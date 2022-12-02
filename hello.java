0b1a97d8d2614cc9a7d29eb33ecec4a8df366f04
sami-eurus <sami.ul.islam@eurustechnologies.com>
2022-12-02 17:19:34 +0500

    commit 3

--- hello.java
+++ hello.java
@@ -4,5 +4,6 @@ public class hello {
         System.out.println("Hello, World!"); 
         System.out.println("Jenkins Git Github!");
         System.out.println("Change 1 in code");
+        System.out.println("Change 2 in code trigger");
     }
 }
